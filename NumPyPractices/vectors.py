1a940d6ae45ff7f9dafa3d4996ee0baf94eab6f8
Tanveer Ahmad <81442859+Tanveer66@users.noreply.github.com>
2023-09-15 01:05:15 +0500

    create file and import all packages that installed

--- NumPyPractices/vectors.py
+++ NumPyPractices/vectors.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pandas as pan
+
 
 # create row_vector
 vector_row = np.array([1, 2, 3])
 
 # create Metrix of 3X3
 matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
+pan.array()
 # this line show first row of matrix
 print(f"first Row of Matrix --> {matrix[0]}")
---
+++ installed Packages.py
@@ -0,0 +1,12 @@
+import scipy as sighPi
+import pandas as pan
+import matplotlib as mat
+import numpy as np
+
+print(np.array([1, 2, 3]))
+
+print(pan.array())
+
+mat.cm
+
+sighPi.test
