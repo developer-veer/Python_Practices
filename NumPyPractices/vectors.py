8cb081c4b2937d553b3ad22b380f0f6da835b285
Tanveer Ahmad <81442859+Tanveer66@users.noreply.github.com>
2023-09-15 00:45:07 +0500

    make some array and martices

---
+++ NumPyPractices/vectors.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# create row_vector
+vector_row = np.array([1, 2, 3])
+
+# create Metrix of 3X3
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# this line show first row of matrix
+print(f"first Row of Matrix --> {matrix[0]}")
